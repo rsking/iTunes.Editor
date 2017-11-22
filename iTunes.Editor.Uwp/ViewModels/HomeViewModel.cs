eea57728b7fcc84a15aa22c17f553274a6435980
Ross King <rsking@gmail.com>
2017-11-23 10:52:38 +1100

    Added Caliburn.Micro to the UWP application

---
+++ iTunes.Editor.Uwp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HomeViewModel.cs" company="RossKing">
+// Copyright (c) RossKing. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ITunes.Editor.ViewModels
+{
+    /// <summary>
+    /// The home view model.
+    /// </summary>
+    internal class HomeViewModel
+    {
+    }
+}
