e1cfdae254f37e48c49925015f4e5345a00c531c
Ross King <rsking@gmail.com>
2018-06-13 11:22:33 +1000

    Added ChartLyrics provider

--- Ninject.Extensions.iTunes.Editor/Module.cs
+++ Ninject.Extensions.iTunes.Editor/Module.cs
@@ -7,11 +7,6 @@
 namespace Ninject.Extensions.ITunes.Editor
 {
     using global::ITunes.Editor;
-    using global::ITunes.Editor.IPod;
-    using global::ITunes.Editor.ITunesLib;
-    using global::ITunes.Editor.Lyrics.Wikia;
-    using global::ITunes.Editor.MediaInfo;
-    using global::ITunes.Editor.PList;
 
     /// <summary>
     /// The Ninject module.
@@ -23,22 +18,23 @@ namespace Ninject.Extensions.ITunes.Editor
         {
             // Songs
             this.Bind<ISongsProvider>().To<FolderSongsProvider>().Named("folder");
-            this.Bind<ISongsProvider>().To<IPodSongsProvider>().Named("ipod");
-            this.Bind<ISongsProvider>().To<PListSongsProvider>().Named("plist");
+            this.Bind<ISongsProvider>().To<global::ITunes.Editor.IPod.IPodSongsProvider>().Named("ipod");
+            this.Bind<ISongsProvider>().To<global::ITunes.Editor.PList.PListSongsProvider>().Named("plist");
             if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
             {
-                this.Bind<ISongsProvider>().To<ITunesSongsProvider>().Named("itunes");
+                this.Bind<ISongsProvider>().To<global::ITunes.Editor.ITunesLib.ITunesSongsProvider>().Named("itunes");
             }
 
             // Tags
             this.Bind<ITagProvider>().To<TagLibTagProvider>().Named("taglib");
-            this.Bind<ITagProvider>().To<MediaInfoTagProvider>().Named("mediainfo");
+            this.Bind<ITagProvider>().To<global::ITunes.Editor.MediaInfo.MediaInfoTagProvider>().Named("mediainfo");
 
             // Composers
             this.Bind<IComposerProvider>().To<ApraAmcosComposerProvider>().Named("apra_amcos");
 
             // Lyrics
-            this.Bind<ILyricsProvider>().To<WikiaLyricsProvider>().Named("wikia");
+            this.Bind<ILyricsProvider>().To<global::ITunes.Editor.Lyrics.Wikia.WikiaLyricsProvider>().Named("wikia");
+            this.Bind<ILyricsProvider>().To<global::ITunes.Editor.ChartLyrics.ChartLyricsProvider>().Named("chart");
 
             // Services
             this.Bind<IUpdateComposerService>().To<UpdateComposerService>();
---
+++ iTunes.Editor.ChartLyrics/ChartLyricsProvider.cs
@@ -0,0 +1,83 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ChartLyricsProvider.cs" company="RossKing">
+// Copyright (c) RossKing. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ITunes.Editor.ChartLyrics
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The <see cref="ILyricsProvider"/> for www.chartlyrics.com
+    /// </summary>
+    public class ChartLyricsProvider : ILyricsProvider
+    {
+        private readonly System.Uri uri = new System.Uri("http://api.chartlyrics.com/apiv1.asmx");
+
+        private readonly apiv1SoapClient channel;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChartLyricsProvider"/> class.
+        /// </summary>
+        public ChartLyricsProvider()
+        {
+            var binding = new System.ServiceModel.BasicHttpBinding { Name = "apiv1Soap" };
+            this.channel = new apiv1SoapClient(binding, new System.ServiceModel.EndpointAddress(this.uri));
+        }
+
+        /// <inheritdoc/>
+        public string GetLyrics(SongInformation tagInformation)
+        {
+            var artist = string.Join("; ", tagInformation.Performers);
+            var songTitle = tagInformation.Title;
+
+            api.chartlyrics.com.GetLyricResult getLyricResult = default;
+            try
+            {
+                getLyricResult = this.channel.SearchLyricDirect(artist, songTitle);
+            }
+            catch (System.ServiceModel.FaultException)
+            {
+                return null;
+            }
+
+            return GetLyricsImpl(artist, songTitle, getLyricResult);
+        }
+
+        /// <inheritdoc/>
+        public async Task<string> GetLyricsAsync(SongInformation tagInformation)
+        {
+            var artist = string.Join("; ", tagInformation.Performers);
+            var songTitle = tagInformation.Title;
+
+            SearchLyricDirectResponse searchLyricDirectResponse = default;
+            try
+            {
+                searchLyricDirectResponse = await this.channel.SearchLyricDirectAsync(artist, songTitle).ConfigureAwait(false);
+            }
+            catch (System.ServiceModel.FaultException)
+            {
+                return null;
+            }
+
+            return GetLyricsImpl(artist, songTitle, searchLyricDirectResponse?.Body?.SearchLyricDirectResult);
+        }
+
+        private static string GetLyricsImpl(string artist, string song, api.chartlyrics.com.GetLyricResult getLyricResult)
+        {
+            if (getLyricResult?.LyricId > 0)
+            {
+                if (getLyricResult.LyricArtist != artist || getLyricResult.LyricSong != song)
+                {
+                    System.Console.WriteLine($"\tIncorrect Lyrics found, Expected {artist}|{song}, but got {getLyricResult.LyricArtist}|{getLyricResult.LyricSong}");
+                    return null;
+                }
+
+                return getLyricResult.Lyric;
+            }
+
+            return null;
+        }
+    }
+}
